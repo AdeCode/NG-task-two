b33a4a43b34d32fabb55d8d70ddcca1e44ee5ac0
Habeeb Agoro <hagoro@vatebra.com>
2025-10-19 12:18:27 +0100

    updated profile controller

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,13 +1,26 @@
 const fetchProfile = async(req,res)=>{
-    //fetch a random fact
-    const response = await fetch('https://catfact.ninja/fact')
-    const fact = await response.json()
-
-    // Current UTC timestamp in ISO 8601
-    const timestamp = new Date().toISOString();
-    
     try{
+        let catFact = null;
         res.setHeader("Content-Type", "application/json");
+
+        try {
+            //fetch a random fact
+            const response = await fetch("https://catfact.ninja/fact");
+      
+            if (!response.ok) {
+              throw new Error(`External API error: ${response.status} ${response.statusText}`);
+            }
+            const data = await response.json();
+            // Ensure the API returned a valid fact
+            if (!data?.fact) {
+              throw new Error("External API returned invalid data");
+            }
+            catFact = data.fact;
+        } catch (error) {
+            console.error("Error fetching cat fact:", error.message);      
+            catFact = "Error fetching cat fact. Please try again later.";
+        }
+
         return res.status(200).json({
             status: "success",
             user:{
@@ -15,10 +28,11 @@ const fetchProfile = async(req,res)=>{
                 name: "Agoro Habeeb Adekorede",
                 stack: "Node.js/Express",
             },
-            timestamp,
-            fact: fact?.fact
+            timestamp: new Date().toISOString(),
+            fact: catFact
         })
     }catch(error){
+        res.setHeader("Content-Type", "application/json");
         return res.status(500).json({
             success: false,
             message: "Error fetching data",
